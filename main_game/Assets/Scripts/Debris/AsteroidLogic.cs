4a0fc88468f6ab0d91792a6c12687d0773f96420
marc_steene <ms13829@my.bristol.ac.uk>
2015-10-28 13:01:42 +0000

    SG-80 Add asteroid attributes

--- main_game/Assets/Scripts/Debris/AsteroidLogic.cs
+++ main_game/Assets/Scripts/Debris/AsteroidLogic.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class AsteroidLogic : MonoBehaviour 
 {
 	GameObject player;
-
-	void Start () 
+	float maxVariation; // Percentage variation in size
+	
+	public void SetPlayer(GameObject temp, float var)
 	{
-		player = GameObject.Find ("PlayerShip");
+		player = temp;
+		maxVariation = var;
+		transform.localScale = new Vector3(10f + Random.Range (-var, var), 10f + Random.Range (-var, var),10f + Random.Range (-var, var));
+		transform.rotation = Random.rotation;
 		StartCoroutine ("DestroyZ");
 	}
 	
--- main_game/Assets/Scripts/Debris/AsteroidSpawner.cs
+++ main_game/Assets/Scripts/Debris/AsteroidSpawner.cs
@@ -6,6 +6,7 @@ public class AsteroidSpawner : MonoBehaviour
 
 	[SerializeField] GameObject asteroid;
 	[SerializeField] int maxAsteroids;
+	[SerializeField] float maxVariation; // Max variation in size (0-10)
 	public static int numAsteroids = 0;
 	
 	// Spawn a new asteroid in a random position if there are less than specified by maxAsteroids
@@ -14,7 +15,8 @@ public class AsteroidSpawner : MonoBehaviour
 		if(numAsteroids < maxAsteroids)
 		{
 			Vector3 rand_position = new Vector3(transform.position.x + Random.Range (-800, 800), transform.position.y + Random.Range (-800, 800), transform.position.z + 150 + Random.Range (50, 1000));
-			Instantiate (asteroid, rand_position, Quaternion.identity);
+			GameObject temp = Instantiate (asteroid, rand_position, Quaternion.identity) as GameObject;
+			temp.GetComponent<AsteroidLogic>().SetPlayer (this.gameObject, maxVariation);
 			numAsteroids += 1;
 		}
 	}
