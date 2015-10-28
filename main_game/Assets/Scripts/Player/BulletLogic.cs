3d649e1dfe169798db28166514f2ccdd4dadc05e
marc_steene <ms13829@my.bristol.ac.uk>
2015-10-28 12:51:54 +0000

    SG-79 Add bullet attributes

--- main_game/Assets/Scripts/Player/BulletLogic.cs
+++ main_game/Assets/Scripts/Player/BulletLogic.cs
@@ -6,13 +6,20 @@ public class BulletLogic : MonoBehaviour
 
 	[SerializeField] float speed = 100f;
 	[SerializeField] float accuracy; // 0 = perfectly accurate, 1 = very inaccurate
+	[SerializeField] Color bulletColor;
+	[SerializeField] float xScale;
+	[SerializeField] float yScale;
+	[SerializeField] float zScale;
 	GameObject player;
 	
 	public void SetPlayer(GameObject temp)
 	{
 		player = temp;
+		transform.localScale = new Vector3(xScale, yScale, zScale);
 		transform.LookAt (player.transform.position); // Set to the correct rotation. Needs position predication
 		transform.Rotate (Random.Range (-accuracy, accuracy), Random.Range (-accuracy, accuracy), Random.Range (-accuracy, accuracy));
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor("_EmissionColor", bulletColor);;
 		StartCoroutine ("DestroyZ");
 	}
 	
