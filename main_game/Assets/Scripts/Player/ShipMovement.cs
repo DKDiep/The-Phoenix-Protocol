cf6e73c2fd9ab0f7904ed3ef2cbfa6e02713aff6
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2015-10-29 18:52:33 +0000

    Solve SG-35: Implement joystick controls

--- main_game/Assets/Scripts/Player/ShipMovement.cs
+++ main_game/Assets/Scripts/Player/ShipMovement.cs
@@ -19,37 +19,64 @@ public class ShipMovement : MonoBehaviour
 	
 	void Update () 
 	{
-		
-		// Detect key presses, ensure velocity is less than some maximum, ensure the angle is constrained between some limits to avoid the player flying backwards
-		if(Input.GetKey (KeyCode.W) && pitchVelocity < 1f && ((transform.eulerAngles.x >= 0 && transform.eulerAngles.x < pitchMax) || transform.eulerAngles.x > 270f))
+        float joyH = Input.GetAxis("Horizontal"), joyV = Input.GetAxis("Vertical");
+
+        // Detect key presses, ensure velocity is less than some maximum, ensure the angle is constrained between some limits to avoid the player flying backwards
+        bool canPitchUp = pitchVelocity < 1f && ((transform.eulerAngles.x >= 0 && transform.eulerAngles.x < pitchMax) || transform.eulerAngles.x > 270f);
+        bool canPitchDown = pitchVelocity > -1f && (transform.eulerAngles.x < 90f || transform.eulerAngles.x > 360f - pitchMin);
+        if (Input.GetKey (KeyCode.W) && canPitchUp)
 		{
 			pitchVelocity += 1f * Time.deltaTime * turnSpeed;
 		}
-		else if(Input.GetKey (KeyCode.S) && pitchVelocity > -1f && (transform.eulerAngles.x < 90f || transform.eulerAngles.x > 360f - pitchMin))
+		else if(Input.GetKey (KeyCode.S) && canPitchDown)
 		{
 			pitchVelocity -= 1f * Time.deltaTime * turnSpeed;
 		}
-		else if(Mathf.Abs (pitchVelocity) > 0) // If no key pressed, decrease velocity
+        else if ((joyV < 0 && canPitchDown) || (joyV > 0 && canPitchUp)) // If keys aren't used, check the joystick
+        {
+            pitchVelocity += joyV * Time.deltaTime * turnSpeed;
+        }
+        else if(Mathf.Abs (pitchVelocity) > 0) // If no key pressed, decrease velocity
 		{
 			pitchVelocity *= 0.5f;
 			if(Mathf.Abs (pitchVelocity) < 0.001f) pitchVelocity = 0f;
 		}
-		
-		if(Input.GetKey (KeyCode.D) && rollVelocity < 1f && ((transform.eulerAngles.y >= 0 && transform.eulerAngles.y < rollMax) || transform.eulerAngles.y > 270f))
+
+        bool canRollRight = rollVelocity < 1f && ((transform.eulerAngles.y >= 0 && transform.eulerAngles.y < rollMax) || transform.eulerAngles.y > 270f);
+        bool canRollLeft = rollVelocity > -1f && (transform.eulerAngles.y < 90f || transform.eulerAngles.y > 360f - rollMin);
+        if (Input.GetKey (KeyCode.D) && canRollRight)
 		{
 			rollVelocity += 1f * Time.deltaTime * turnSpeed;
 		}
-		else if(Input.GetKey (KeyCode.A) && rollVelocity > -1f && (transform.eulerAngles.y < 90f || transform.eulerAngles.y > 360f - rollMin))
+		else if(Input.GetKey (KeyCode.A) && canRollLeft)
 		{
 			rollVelocity -= 1f * Time.deltaTime * turnSpeed;
 		}
+        else if ((joyH < 0 && canRollLeft) || (joyH > 0 && canRollRight))
+        {
+            rollVelocity += joyH * Time.deltaTime * turnSpeed;
+        }
 		else if(Mathf.Abs (rollVelocity) > 0)
 		{
 			rollVelocity *= 0.5f;
 			if(Mathf.Abs (rollVelocity) < 0.001f) rollVelocity = 0f;
 		}
-		
-		pitchAngle = transform.eulerAngles.x + pitchVelocity;
+
+        
+        //Debug.Log(joyH + " " + joyV);
+        /*if ((joyH > 0 && rollVelocity < 1f && ((transform.eulerAngles.y >= 0 && transform.eulerAngles.y < rollMax) || transform.eulerAngles.y > 270f))
+            || (joyH < 0 && rollVelocity > -1f && (transform.eulerAngles.y < 90f || transform.eulerAngles.y > 360f - rollMin)))
+        {
+            rollVelocity += joyH * Time.deltaTime * turnSpeed;
+        }
+        else if (Mathf.Abs(rollVelocity) > 0)
+        {
+            rollVelocity *= 0.5f;
+            if (Mathf.Abs(rollVelocity) < 0.001f) rollVelocity = 0f;
+        }*/
+
+
+        pitchAngle = transform.eulerAngles.x + pitchVelocity;
 		rollAngle = transform.eulerAngles.y + rollVelocity;
 		driftVelocity = transform.eulerAngles.z % 90f; // Fixes wrapping of angles
 		if(transform.eulerAngles.z > 310f) driftVelocity *= (-1f * sidewaysSpeed * Time.deltaTime);
