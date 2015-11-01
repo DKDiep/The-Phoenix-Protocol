335974babda0084f8f278f947defd06f935f22ae
ArchArch <dd13282@my.bristol.ac.uk>
2015-11-01 08:21:55 +0000

    SG-75: complete basic example of transferring data between scenes

--- main_game/Assets/Scripts/Player/PlayerControl.cs
+++ main_game/Assets/Scripts/Player/PlayerControl.cs
@@ -1,15 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    [SyncVar]
+    public float shipX;
+
+    [SyncVar]
+    public float shipZ;
+
+    private GameObject ship;
+
+	void Start ()
+    {
+        ship = GameObject.Find("PlayerShip");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+    {
+	    if (ship != null)
+        {
+            //Debug.Log("Ship identified");
+        }
 	}
+
+    void OnGUI()
+    {
+        if (ship != null)
+        {
+            GUI.Label(new Rect(10, 200, 200, 20), "Ship identified");
+            shipX = ship.transform.position.x;
+            shipZ = ship.transform.position.z;
+        }
+        else
+        {
+            GUI.Label(new Rect(10, 200, 200, 20), "Ship unidentified");
+        }
+        GUI.Label(new Rect(10, 220, 200, 20), "ShipX: " + shipX);
+        GUI.Label(new Rect(10, 240, 200, 20), "ShipZ: " + shipZ);
+    }
 }
+
+/*
+public class PlayerControl : NetworkBehaviour
+{
+	static int s_playerId;
+
+	[SyncVar]
+	public int playerId;
+
+	[SyncVar]
+	public int score;
+
+	[Command]
+	void CmdScore(int amount)
+	{
+		score += amount;
+	}
+
+	void Awake()
+	{
+		playerId = s_playerId++;
+	}
+
+	void OnGUI()
+	{
+		if (isLocalPlayer)
+		{
+			if (GUI.Button(new Rect(50, 200 + playerId * 30, 200, 20), "Your Score:" + score))
+			{
+				CmdScore(10);
+			}
+		}
+		else
+		{
+			GUI.Label(new Rect(50, 200 + playerId * 30, 200, 20), "Other Score:" + score);
+		}
+	}
+
+}
+*/
