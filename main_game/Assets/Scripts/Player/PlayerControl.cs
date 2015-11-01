c689fac8656c2a9582c1ad2ca7255457760da660
ArchArch <dd13282@my.bristol.ac.uk>
2015-11-01 08:29:05 +0000

    SG-75: add comments; renamed StartScene to TestNetworkScene

--- main_game/Assets/Scripts/Player/PlayerControl.cs
+++ main_game/Assets/Scripts/Player/PlayerControl.cs
@@ -4,32 +4,29 @@ using UnityEngine.Networking;
 
 public class PlayerControl : NetworkBehaviour {
 
+    // Sync vars are synchronised for all connected instances of scripts
     [SyncVar]
     public float shipX;
 
     [SyncVar]
     public float shipZ;
 
+    // Private to each instance of script
     private GameObject ship;
 
 	void Start ()
     {
+        // Look for gameObject called "PlayerShip", returns null if not found. MainScene will find, TestNetworkScene won't.
         ship = GameObject.Find("PlayerShip");
 	}
 
-	void Update ()
-    {
-	    if (ship != null)
-        {
-            //Debug.Log("Ship identified");
-        }
-	}
-
+    // OnGUI draws to screen and is called every few frames
     void OnGUI()
     {
         if (ship != null)
         {
             GUI.Label(new Rect(10, 200, 200, 20), "Ship identified");
+            //Set the sync vars, this is set on MainScene, and then TestNetworkScene will be synced.
             shipX = ship.transform.position.x;
             shipZ = ship.transform.position.z;
         }
@@ -41,43 +38,3 @@ public class PlayerControl : NetworkBehaviour {
         GUI.Label(new Rect(10, 240, 200, 20), "ShipZ: " + shipZ);
     }
 }
-
-/*
-public class PlayerControl : NetworkBehaviour
-{
-	static int s_playerId;
-
-	[SyncVar]
-	public int playerId;
-
-	[SyncVar]
-	public int score;
-
-	[Command]
-	void CmdScore(int amount)
-	{
-		score += amount;
-	}
-
-	void Awake()
-	{
-		playerId = s_playerId++;
-	}
-
-	void OnGUI()
-	{
-		if (isLocalPlayer)
-		{
-			if (GUI.Button(new Rect(50, 200 + playerId * 30, 200, 20), "Your Score:" + score))
-			{
-				CmdScore(10);
-			}
-		}
-		else
-		{
-			GUI.Label(new Rect(50, 200 + playerId * 30, 200, 20), "Other Score:" + score);
-		}
-	}
-
-}
-*/
