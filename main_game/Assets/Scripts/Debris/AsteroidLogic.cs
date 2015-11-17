5e16b6c1a6ffb19890cc18ef0468b30dbacc7fab
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2015-11-17 13:36:36 +0000

    SG-74: Fix asteroids being destroyed from both GameState and own logic script

--- main_game/Assets/Scripts/Debris/AsteroidLogic.cs
+++ main_game/Assets/Scripts/Debris/AsteroidLogic.cs
@@ -12,18 +12,5 @@ public class AsteroidLogic : MonoBehaviour
 		maxVariation = var;
 		transform.localScale = new Vector3(10f + Random.Range (-var, var), 10f + Random.Range (-var, var),10f + Random.Range (-var, var));
 		transform.rotation = Random.rotation;
-		StartCoroutine ("DestroyZ");
-	}
-	
-	// Automatically destroy if 100 units behind the player
-	IEnumerator DestroyZ()
-	{
-		yield return new WaitForSeconds(1f);
-		if(transform.position.z < player.transform.position.z - 100f)
-		{
-			AsteroidSpawner.numAsteroids -= 1;
-			Destroy (this.gameObject);
-		}
-		StartCoroutine ("DestroyZ");
 	}
 }
