a3b9e19c7e198451185655568e02c30982aa9892
dillon_diep <dd13282@my.bristol.ac.uk>
2015-11-20 18:26:55 +0000

    SG-121: add host player prefab

---
+++ main_game/Assets/Scripts/Player/HostController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HostController : MonoBehaviour {
+
+    private GameObject pawn;
+
+    GameObject GetControlledPawn()
+    {
+        return pawn;
+    }
+
+    void SetControlledPawn(GameObject pawnObject)
+    {
+        pawn = pawnObject;
+    }
+}
