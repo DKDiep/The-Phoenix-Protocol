d4547d270e3bc3c9dc28cab35cc058f75bd74adc
Artur <csika94@hotmail.com>
2015-11-26 16:28:32 +0000

    Renamed FPS Controller prefab to Engineer prefab. Started engineer spawner

---
+++ main_game/Assets/EngineerSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineerSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject engineer;
+    [SerializeField]
+    GameObject gameManager;
+
+    private GameState gameState;
+    private ServerManager serverManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (gameManager != null)
+        {
+            gameState = gameManager.GetComponent<GameState>();
+            serverManager = gameManager.GetComponent<ServerManager>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
