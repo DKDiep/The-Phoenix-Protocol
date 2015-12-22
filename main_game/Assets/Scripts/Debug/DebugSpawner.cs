4bf01ac5c5fdd189fb53ec63b8f909658d776b82
marc_steene <ms13829@my.bristol.ac.uk>
2015-12-22 13:55:22 +0300

    Remove Engineer prefab and spawner script. Add debug script to server and client

---
+++ main_game/Assets/Scripts/Debug/DebugSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugSpawner : MonoBehaviour {
+
+	[SerializeField]
+	GameObject gameManager;
+	private GameState state;
+	[SerializeField] GameObject myCounter;
+	bool debug = false;
+
+	// Use this for initialization
+	void Start () {
+		if (gameManager != null)
+		{
+			state = gameManager.GetComponent<GameState>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (state.GetStatus() == GameState.Status.Started)
+		{
+			if(!debug)
+			{
+				myCounter.SetActive (true);
+				debug = true;
+			}
+		}
+	}
+}
