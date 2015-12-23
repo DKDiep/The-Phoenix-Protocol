d22a94c615dc86dd80fbef6d5cfaf83acd56efa9
Artur <csika94@hotmail.com>
2015-12-23 20:53:01 +0000

    SG-125, Add serverside implementation of networked engineers

---
+++ main_game/Assets/Scripts/Engineer/PositionAndRotationSync.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PositionAndRotationSync : NetworkBehaviour
+{
+    [SyncVar]
+    Quaternion rotation;
+
+    [SyncVar]
+    Vector3 position;
+
+    void Update()
+    {
+        if (isServer)
+        {
+            //Debug.Log("server");
+            rotation = gameObject.transform.rotation;
+            position = gameObject.transform.position;
+        }
+        else if (isClient)
+        {
+            //Debug.Log("client");
+            gameObject.transform.rotation = rotation;
+            gameObject.transform.position = position;
+        }
+    }
+}
