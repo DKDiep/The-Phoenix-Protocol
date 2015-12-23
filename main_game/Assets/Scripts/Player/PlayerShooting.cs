6f86c6b56de37a94b988bf1f36b7d207eb118ca6
marc_steene <ms13829@my.bristol.ac.uk>
2015-12-23 12:34:27 +0300

    Add fully automatic shooting with rate of fire parameter

--- main_game/Assets/Scripts/Player/PlayerShooting.cs
+++ main_game/Assets/Scripts/Player/PlayerShooting.cs
@@ -6,13 +6,15 @@ public class PlayerShooting : MonoBehaviour
 
 	[SerializeField] GameObject bullet;
 	[SerializeField] GameObject bulletLogic;
-	[SerializeField] float xOffset, yOffset, zOffset;
+	[SerializeField] float xOffset, yOffset, zOffset, rateOfFire;
 	GameObject bulletAnchor;
 	GameObject target;
+	bool canShoot;
 
 	// Use this for initialization
 	void Start () 
 	{
+		canShoot = true;
 		target = new GameObject();
 		transform.localPosition = new Vector3(0,0,0);
 		foreach(Transform child in transform.parent)
@@ -27,7 +29,7 @@ public class PlayerShooting : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetMouseButtonDown (0))
+		if(Input.GetMouseButton (0) && canShoot)
 		{
 			target.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(CrosshairMovement.crosshairPosition.x, CrosshairMovement.crosshairPosition.y, 1000));
 			target.transform.Translate (transform.forward * (-10f));
@@ -36,6 +38,14 @@ public class PlayerShooting : MonoBehaviour
 			logic.transform.parent = obj.transform;
 			logic.GetComponent<BulletLogic>().SetDestination (target);
 			ServerManager.NetworkSpawn(obj);
+			canShoot = false;
+			StartCoroutine("Delay");
 		}
+	}
+
+	IEnumerator Delay()
+	{
+		yield return new WaitForSeconds(rateOfFire);
+		canShoot = true;
 	}
 }
