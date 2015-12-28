415f34c5f35166fb8aff892e041c72d856c47e6c
Artur <csika94@hotmail.com>
2015-12-28 15:04:20 +0000

    SG-169, Implement spawning engineer on client join instead of game start

--- main_game/Assets/Scripts/Engineer/EngineerMenu.cs
+++ main_game/Assets/Scripts/Engineer/EngineerMenu.cs
@@ -7,7 +7,7 @@ public class EngineerMenu : MonoBehaviour {
     [SerializeField] GameObject menuCam;
     [SerializeField] GameObject debug;
 
-    GameObject ship;
+    NetworkClient client;
     GameState gameState;
     NetworkManager manager;
 
@@ -17,20 +17,44 @@ public class EngineerMenu : MonoBehaviour {
         gameState = gameObject.GetComponent<GameState>();
         manager = gameObject.GetComponent<NetworkManager>();
     }
-	
-	// Sketchy method to detect when client game has started. Needs replacing.
-	void Update () 
-	{
-		ship = GameObject.Find ("PlayerShip(Clone)");
-	}
 
     void OnGUI()
 	{
-        if (GUI.Button(new Rect((Screen.width / 2) - 75, (Screen.height / 2) - 50, 150, 100), "Start Game") && ship != null)
+        if (client == null)
         {
-            Destroy(menuCam);
-            debug.SetActive(true);
-            Destroy(this);
+            if (GUI.Button(new Rect((Screen.width / 2) - 75, (Screen.height / 2) - 50, 150, 100), "Start Game"))
+            {
+                client = manager.StartClient();
+
+                //if (client != null)
+                //{
+                //    Debug.Log("Waiting for game to start");
+                //    System.Threading.Thread.Sleep(5000);
+                //    ship = GameObject.Find("PlayerShip(Clone)");
+                //}
+                //
+                //Debug.Log("Game started");
+                //if (ship != null)
+                //{
+                //    Destroy(menuCam);
+                //    debug.SetActive(true);
+                //    Destroy(this);
+                //
+                //    //Disable all cameras so that the engineer camera will be the main camera
+                //    foreach (Camera cam in Camera.allCameras)
+                //    {
+                //        cam.enabled = false;
+                //    }
+                //
+                //    //Disable all audiolisteners associated with the ship
+                //    foreach (AudioListener l in ship.GetComponentsInChildren<AudioListener>())
+                //    {
+                //        l.enabled = false;
+                //    }
+                //
+                //    joinHandler.CmdEngineerJoin();
+                //}
+            }
         }
 	}
 }
