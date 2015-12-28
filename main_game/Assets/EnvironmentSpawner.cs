f71624dc4057cc643d85c01f5709a543d94ed7bd
marc_steene <ms13829@my.bristol.ac.uk>
2015-12-28 11:52:35 +0300

    SG-166 Spawn space scene environment across network

---
+++ main_game/Assets/EnvironmentSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentSpawner : MonoBehaviour 
+{
+
+  [SerializeField] GameObject spaceScene;
+  GameState state;
+
+	// Use this for initialization
+	void Start () 
+  {
+    state = transform.parent.gameObject.GetComponent<GameState>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    if (state.GetStatus() == GameState.Status.Started)
+        {
+          Instantiate(spaceScene, Vector3.zero, Quaternion.identity);
+          ServerManager.NetworkSpawn(spaceScene);
+          Destroy(this);
+        }
+	}
+}
