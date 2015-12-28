c5c8b7d5a4d5fc0994f0ff33b6f96d6f7366538d
marc_steene <ms13829@my.bristol.ac.uk>
2015-12-28 14:33:18 +0300

    SG-173 Add interpolated network transforms and networked scaling

---
+++ main_game/Assets/Scripts/Network/UScaleSync.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class UScaleSync : NetworkBehaviour
+{
+
+  [SyncVar] Vector3 scale;
+
+  void Start ()
+  {
+      if(isServer)
+       {
+            scale = gameObject.transform.localScale;
+       }
+       else if (isClient)
+       {
+            gameObject.transform.localScale = scale;
+       }
+  }
+}
