59dff380c824867c254af4e20d5ea658644aef33
marc_steene <ms13829@my.bristol.ac.uk>
2015-12-29 21:51:17 +0300

    SG-183 Add game timer

---
+++ main_game/Assets/Scripts/GUI/TimerScript.cs
@@ -0,0 +1,30 @@
+﻿/*
+    2015-2016 Team Pyrolite
+    Project "Sky Base"
+    Authors: Marc Steene
+    Description: Counts number of seconds survived
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour 
+{
+
+    Text counterText;
+    public float minutes, seconds;
+
+	// Use this for initialization
+	void Start () 
+    {
+        counterText = GetComponent<Text>();
+	}
+
+    void Update()
+    {
+        seconds = Time.timeSinceLevelLoad % 60f;
+        minutes = Time.timeSinceLevelLoad / 60f;
+        counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00.00");
+    }
+}
