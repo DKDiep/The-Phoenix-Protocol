fc6e96bfea55b3699187c752ac5b40393f1f658d
marc_steene <ms13829@my.bristol.ac.uk>
2016-01-27 14:38:42 +0000

    SG-221 Bound Health

--- main_game/Assets/Scripts/Player/DamageEffects.cs
+++ main_game/Assets/Scripts/Player/DamageEffects.cs
@@ -52,7 +52,7 @@ public class DamageEffects : NetworkBehaviour
 	public void Damage(int dir, float damage, float hp)
 	{
         health = hp;
-        lowHealth.amount = Mathf.Clamp(0.25f - ((float)health/100f),0f,0.25f) * 2f;
+        lowHealth.amount = Mathf.Clamp(0.25f - ((float)health/100f),0f,0.25f) * 1.5f;
 		direction = dir;
 		alpha = Mathf.Clamp(0.5f + (damage/20f),0f,1f);
         //RpcUpdateClientDamage(dir,damage,hp);
@@ -62,7 +62,7 @@ public class DamageEffects : NetworkBehaviour
     void RpcUpdateClientDamage(int dir, float damage, float hp)
     {
         health = hp;
-        lowHealth.amount = Mathf.Clamp(0.25f - ((float)health/100f),0f,0.25f) * 2f;
+        lowHealth.amount = Mathf.Clamp(0.25f - ((float)health/100f),0f,0.25f) * 1.5f;
         direction = dir;
         alpha = Mathf.Clamp(0.5f + (damage/20f),0f,1f);
     }
