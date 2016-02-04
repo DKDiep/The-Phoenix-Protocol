53204e75a86d273eb0473e23ba079b26da69b3af
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-02-04 19:31:48 +0000

    SG-265 Turrets now follow crosshair rather than the mouse pointer.
    This enables the turrets to move with the wii remote rather than just the mouse pointer.

--- main_game/Assets/Scripts/Player/FollowReticule.cs
+++ main_game/Assets/Scripts/Player/FollowReticule.cs
@@ -10,19 +10,34 @@ using System.Collections;
 
 public class FollowReticule : MonoBehaviour 
 {
+	// Empty game object to use as the target position
+    GameObject targetPoint; 
 
-    GameObject targetPoint; // Empty game object to use as the target position
+	GameObject[] crosshairs;
 
     // Initialise temporary object
     void Start()
     {
         targetPoint = new GameObject();
         targetPoint.name = "AimTarget";
+
+		GameObject crosshairContainer = GameObject.Find("Crosshairs");
+
+		crosshairs = new GameObject[4];
+
+		// Find crosshair images
+		for(int i = 0; i < 4; ++i)
+		{
+			crosshairs[i] = crosshairContainer.transform.GetChild(i).gameObject;
+		}
+
+
     }
  
     void FixedUpdate () 
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// Currently only for player 0, needs to be updated for each player. 
+		Ray ray = Camera.main.ScreenPointToRay(crosshairs[0].transform.position);
         RaycastHit hit;
 
         if(Physics.Raycast(ray,out hit) && !hit.transform.gameObject.tag.Equals("Player"))
@@ -31,7 +46,7 @@ public class FollowReticule : MonoBehaviour
         }
         else
         {
-            targetPoint.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1000));
+			targetPoint.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(crosshairs[0].transform.position.x, crosshairs[0].transform.position.y, 1000));
             targetPoint.transform.Translate (transform.parent.transform.forward * (-10f));
         }
 
