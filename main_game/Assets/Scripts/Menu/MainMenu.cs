90ca2453c6295dfb998e4b91d9cf99589cca0866
Artur <csika94@hotmail.com>
2016-02-05 19:38:05 +0000

    SG-273, Remove unnecessary Join(Engineer) button and corresponding functionality

--- main_game/Assets/Scripts/Menu/MainMenu.cs
+++ main_game/Assets/Scripts/Menu/MainMenu.cs
@@ -20,7 +20,6 @@ public class MainMenu : NetworkBehaviour
     void Start()
     {
         manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
-        messageHandler = manager.transform.GetComponent<MessageHandler>();
     }
 
     public void CreateGame()
@@ -33,23 +32,6 @@ public class MainMenu : NetworkBehaviour
     {
         startServer = false;
         manager.StartClient();
-    }
-
-    public void JoinAsEngineer()
-    {
-        startServer = false;
-        NetworkClient client = manager.StartClient();
-
-        //Set the client for the message handler
-        messageHandler.SetClient(client);
-
-        // Register a handler for Owner messages. This message
-        // tells the client which object it controls
-        client.RegisterHandler(890, messageHandler.OnServerOwner);
-
-        // Register handler for connection message. This is received when the
-        // server and client establish a connection
-        client.RegisterHandler(MsgType.Connect, messageHandler.OnClientConnect);
     }
 
     public void ShowOptions()
