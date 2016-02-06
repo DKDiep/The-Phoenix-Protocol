885efeccc97193ab35cd441c76014012d0378d3c
marc_steene <ms13829@my.bristol.ac.uk>
2016-02-06 18:35:52 +0000

    SG-286 Replace Tonemapping

---
+++ main_game/Assets/Standard Assets/Effects/ImageEffects/Tonemapping/Editor/DeluxeTonemapperEditor.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(DeluxeTonemapper))]
+class DeluxeTonemapperEditor : Editor
+{
+    Texture2D m_Logo;
+
+    enum Presets
+    {
+        ChoosePreset,
+        LDR_Neutral,
+        LDR_Soft,
+        LDR_Contrasted1,
+        LDR_Contrasted2,
+        HDR_Neutral,
+        HDR_Soft,
+        HDR_Contrasted1,
+        HDR_Contrasted2
+    }
+
+    void OnEnable()
+    {
+        MonoScript script = MonoScript.FromScriptableObject(this);
+        string path = AssetDatabase.GetAssetPath(script);
+        string logoPath = Path.GetDirectoryName(path) + "/logo.png";
+        //m_Logo = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Paroxe/FilmicTonemappingDeluxe/Editor/logo.png", typeof(Texture2D));
+        m_Logo = (Texture2D)AssetDatabase.LoadAssetAtPath(logoPath, typeof(Texture2D));
+
+        if (m_Logo != null)
+            m_Logo.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    public override void OnInspectorGUI () 
+    {
+
+        DeluxeTonemapper dt = (DeluxeTonemapper)target;
+        Undo.RecordObject(dt, "Deluxe Tonemapper");
+
+        if (m_Logo != null)
+        {
+            Rect rect = GUILayoutUtility.GetRect(m_Logo.width, m_Logo.height);
+            //GUI.DrawTexture(rect, m_Background);
+            GUI.DrawTexture(rect, m_Logo, ScaleMode.ScaleToFit);
+        }
+
+        EditorGUILayout.LabelField("Filmic Curve", EditorStyles.boldLabel);
+        dt.m_MainCurve.OnGUI(dt.m_Mode);
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Curve Settings", EditorStyles.boldLabel);
+
+        Presets preset = (Presets)EditorGUILayout.EnumPopup("Presets", Presets.ChoosePreset);
+        if (preset != Presets.ChoosePreset)
+            SetPreset(preset, dt);
+
+        dt.m_Mode = (DeluxeTonemapper.Mode)EditorGUILayout.EnumPopup("Mode", dt.m_Mode);
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Color Correction", EditorStyles.boldLabel);
+        dt.m_Tint = EditorGUILayout.ColorField("Tint", dt.m_Tint);
+
+        
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+
+    }
+
+    void SetPreset(Presets preset, DeluxeTonemapper tonemapper)
+    {
+        if (preset == Presets.LDR_Neutral)
+            Set(tonemapper, 0.0f, 0.5f, 0.0f, 0.0f, 1.0f);
+        else if (preset == Presets.LDR_Contrasted1)
+            Set(tonemapper, 0.518f, 0.1f, 0.0f, 0.54f, 1.0f);
+        else if (preset == Presets.LDR_Contrasted2)
+            Set(tonemapper, 0.5f, 0.1f, 0.0f, 0.5f, 1.0f);
+        else if (preset == Presets.LDR_Soft)
+            Set(tonemapper, -0.2f, 0.2f, 0.0f, 0.4f, 1.0f);
+        else if (preset == Presets.HDR_Neutral)
+            Set(tonemapper, 0.0f, 0.5f, 0.0f, 0.0f, 4.0f);
+        else if (preset == Presets.HDR_Soft)
+            Set(tonemapper, 0.127f, 0.3f, 0.0f, 0.833f, 4.0f);
+        else if (preset == Presets.HDR_Contrasted1)
+            Set(tonemapper, 0.402f, 0.04f, 0.0f, 0.9f, 4.0f);
+        else if (preset == Presets.HDR_Contrasted2)
+            Set(tonemapper, 0.385f, 0.12f, 0.0f, 0.883f, 4.0f);
+    }
+
+    void Set(DeluxeTonemapper dt, float t, float c, float b, float s, float w)
+    {
+        dt.m_MainCurve.m_ToeStrength = t;
+        dt.m_MainCurve.m_CrossOverPoint = c;
+        dt.m_MainCurve.m_BlackPoint = b;
+        dt.m_MainCurve.m_ShoulderStrength = s;
+        dt.m_MainCurve.m_WhitePoint = w;
+        dt.UpdateCoefficients();
+    }
+
+
+    [MenuItem("Component/Filmic Tonemapping/Add Tonemapper")]
+    public static void AddPreset()
+    {
+        AddTonamappingComponent();
+    }
+
+
+    static void AddTonamappingComponent()
+    {
+        GameObject obj = Selection.activeGameObject;
+        if (obj == null)
+            return;
+        if (obj.GetComponent<Camera>() == null)
+            return;
+
+        obj.AddComponent<DeluxeTonemapper>();
+    }
+
+    
+
+
+}
---
+++ main_game/Assets/Standard Assets/Effects/ImageEffects/Tonemapping/Scripts/DeluxeTonemapper.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class DeluxeTonemapper : MonoBehaviour 
+{
+    [SerializeField]
+    public FilmicCurve m_MainCurve = new FilmicCurve();
+
+    [SerializeField]
+    public Color m_Tint = new Color(1,1,1,1);
+
+    [SerializeField]
+    public Mode m_Mode;
+
+
+    private Mode m_LastMode;
+    Material m_Material;
+    Shader m_Shader;
+
+
+    public enum Mode
+    {
+        Color,
+        Luminance,
+        ExtendedLuminance
+    }
+
+
+    public Shader TonemappingShader
+    {
+        get { return m_Shader; }
+    }
+
+    private void DestroyMaterial(Material mat)
+    {
+        if (mat)
+        {
+            DestroyImmediate(mat);
+            mat = null;
+        }
+    }
+
+    private void CreateMaterials()
+    {
+        if (m_Shader == null)
+        {
+            if (m_Mode == Mode.Color)
+                m_Shader = Shader.Find("Hidden/Deluxe/TonemapperColor");
+            if (m_Mode == Mode.Luminance)
+                m_Shader = Shader.Find("Hidden/Deluxe/TonemapperLuminosity");
+            if (m_Mode == Mode.ExtendedLuminance)
+                m_Shader = Shader.Find("Hidden/Deluxe/TonemapperLuminosityExtended");
+        }
+
+        if (m_Material == null && m_Shader != null && m_Shader.isSupported)
+        {
+            m_Material = CreateMaterial(m_Shader);
+        }
+    }
+
+    private Material CreateMaterial(Shader shader)
+    {
+        if (!shader)
+            return null;
+        Material m = new Material(shader);
+        m.hideFlags = HideFlags.HideAndDontSave;
+        return m;
+    }
+
+    void OnDisable()
+    {
+        DestroyMaterial(m_Material); m_Material = null; m_Shader = null;
+    }
+
+    public void StoreK()
+    {
+        m_MainCurve.StoreK();
+    }
+
+    public void UpdateCoefficients()
+    {
+        StoreK();
+        m_MainCurve.UpdateCoefficients();
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_K", m_MainCurve.m_k);
+        m_Material.SetFloat("_Crossover", m_MainCurve.m_CrossOverPoint);
+        m_Material.SetVector("_Toe", m_MainCurve.m_ToeCoef);
+        m_Material.SetVector("_Shoulder", m_MainCurve.m_ShoulderCoef);
+        m_Material.SetVector("_Tint", m_Tint);
+        m_Material.SetFloat("_LuminosityWhite", m_MainCurve.m_LuminositySaturationPoint * m_MainCurve.m_LuminositySaturationPoint);
+    }
+
+    public void ReloadShaders()
+    {
+        OnDisable();
+    }
+
+    void OnEnable()
+    {
+        if (m_MainCurve == null)
+            m_MainCurve = new FilmicCurve();
+        CreateMaterials();
+        UpdateCoefficients();
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (m_Mode != m_LastMode)
+            ReloadShaders();
+        m_LastMode = m_Mode;
+
+        CreateMaterials();
+        UpdateCoefficients();
+
+        Graphics.Blit(source, destination, m_Material);
+
+        
+    }
+}
