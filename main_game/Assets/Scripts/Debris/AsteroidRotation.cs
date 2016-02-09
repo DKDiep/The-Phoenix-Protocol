54da769a38a12b5279bad92bad863d8a0b7d0490
marc_steene <ms13829@my.bristol.ac.uk>
2016-02-09 13:52:56 +0000

    SG-300 Add Asteroid LOD

--- main_game/Assets/Scripts/Debris/AsteroidRotation.cs
+++ main_game/Assets/Scripts/Debris/AsteroidRotation.cs
@@ -13,16 +13,40 @@ public class AsteroidRotation : NetworkBehaviour
 {
 
   [SyncVar] float speed;
+  GameObject player;
+  [SerializeField] Mesh highPoly;
+  [SerializeField] Mesh medPoly;
+  [SerializeField] Mesh lowPoly;
+  MeshFilter myFilter;
 
   // Only one packet needs to be sent to the client to control the asteroid's rotation
   void Start ()
   {
     if(isServer) speed = GetComponentInChildren<AsteroidLogic>().speed;
+    player = GameObject.Find("PlayerShip(Clone)");
+    myFilter = GetComponent<MeshFilter>();
+    StartCoroutine("AsteroidLOD");
   }
 	
   void Update()
   {
     transform.Rotate(transform.forward * speed * Time.deltaTime);
   }
+
+  IEnumerator AsteroidLOD()
+  {
+    float distance = Vector3.Distance(transform.position, player.transform.position);
+    if(distance < 250)
+        myFilter.mesh = highPoly;
+    else if(distance < 500)
+        myFilter.mesh = medPoly;
+    else
+        myFilter.mesh = lowPoly;
+
+    yield return new WaitForSeconds(1f);
+    StartCoroutine("AsteroidLOD");
+
+  }
+
 
 }
