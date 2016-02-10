c9cfa3abd3bc934671f01b6007c360d40dd32029
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-02-10 16:20:37 +0000

    SG-250 Add dummy portal for victory

---
+++ main_game/Assets/Scripts/Portal/PortalCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalCollision : MonoBehaviour 
+{
+	PortalLogic portalLogic;
+
+	void Start()
+	{
+		portalLogic = GetComponentInChildren<PortalLogic>();
+	}
+		
+	void OnTriggerEnter (Collider col)
+	{
+			portalLogic.collision();
+	}
+}
+
---
+++ main_game/Assets/Scripts/Portal/PortalLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalLogic : MonoBehaviour {
+
+	public float distanceToWin = 0.9f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void collision() 
+	{
+		Debug.Log("You have reached the portal. You have won then game!");
+	}
+}
