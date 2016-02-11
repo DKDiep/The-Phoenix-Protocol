d89133facfb9d81a925a435a3feec13cf8432a18
lukebryant <mb13273@my.bristol.ac.uk>
2016-02-11 13:40:57 +0000

    SG-314 Send dummy data to server periodically

---
+++ main_game/Assets/Scripts/Network/GameStatsManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStatsManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        if (MainMenu.startServer)
+        {
+            StartCoroutine(SendRequest());
+        }
+    }
+
+    IEnumerator SendRequest()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            string url = "http://localhost:8080/bar";
+            WWWForm form = new WWWForm();
+            form.AddField("Future metacritic rating", "10");
+            WWW www = new WWW(url, form);
+            yield return www;
+            if (www.error == null)
+            {
+                //Debug.Log("WWW Ok!: " + www.data);
+            }
+            else
+            {
+                Debug.Log("WWW Error: " + www.error);
+            }
+            new WaitForSeconds(5.0f);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
