c1de946538a793ff44b1e22d3022f4714a6eefd4
Artur <csika94@hotmail.com>
2016-02-24 19:13:37 +0000

    SG-92-2, Remove un-necessary explanatory comment

--- main_game/Assets/Scripts/Player/EngineerInteraction.cs
+++ main_game/Assets/Scripts/Player/EngineerInteraction.cs
@@ -6,9 +6,6 @@ public class EngineerInteraction : NetworkBehaviour {
     private bool upgradeable = false;
     private bool repairable = false;
 
-    // Not using auto properties for getters and setters
-    // because the interaction between auto properties and
-    // sync vars is unknown
     public void setUpgradeable(bool value)
     {
         upgradeable = value;
