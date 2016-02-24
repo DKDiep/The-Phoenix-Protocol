c5db928139f67e5df5f2ba742cdb51f5b07744b5
marc_steene <ms13829@my.bristol.ac.uk>
2016-02-24 16:40:44 +0000

    SG-342 Replace enemy target system with 3D system

---
+++ main_game/Assets/TargetScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetScript : MonoBehaviour 
+{
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    player = GameObject.Find("PlayerShip(Clone)");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+            Vector3 v3 = player.transform.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(-v3);
+	}
+}
