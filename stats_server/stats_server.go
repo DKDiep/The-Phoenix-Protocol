ff83bf04b3b14193d9c6403c2b0e06b68ff48796
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-02-25 16:38:20 +0000

    SG-36 Move live stats to main repo

---
+++ stats_server/stats_server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"golang.org/x/net/html"
+	"log"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	JSONString := ""
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		// fmt.Printf("Cello, %q", html.EscapeString(r.URL.Path))
+
+		byteArray, err1 := ioutil.ReadAll(r.Body);
+		decodedString, err2 := url.QueryUnescape(string(byteArray));
+		if err1 == nil {
+			if err2 == nil {
+				JSONString = strings.TrimPrefix(decodedString,"JSON:=")
+				fmt.Println(decodedString)
+			}	else{
+		    log.Fatal(err2)
+			}
+		}	else{
+			log.Fatal(err1)
+		}
+
+		// hah, err := ioutil.ReadAll(r.Body);
+		// if err != nil {
+		// 	fmt.Fprintf(w, "%s", err)
+		// }
+		// fmt.Fprintf(w,"%s",hah)
+	})
+	http.Handle("/", http.FileServer(http.Dir("../stats_web")))
+	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "%q", JSONString)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
