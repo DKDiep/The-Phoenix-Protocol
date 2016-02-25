2b7fbf4d6d3feed2cf4b67f15f7e89b0f346768a
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-02-25 17:13:49 +0000

    SG-36 Add fonts file to setup script

--- web/setup_resources.sh
+++ web/setup_resources.sh
@@ -1,2 +1,6 @@
 ln -s ../resources/include/ ./phone_web/include
 ln -s ../resources/include/ ./stats_web/include
+
+
+ln -s ../resources/fonts/ ./phone_web/fonts
+ln -s ../resources/fonts/ ./stats_web/fonts
