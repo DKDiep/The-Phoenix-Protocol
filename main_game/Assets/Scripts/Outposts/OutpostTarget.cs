a81ca2743c6e247d1b043b03dc26df637b0b8767
marc_steene <ms13829@my.bristol.ac.uk>
2016-02-28 15:45:30 +0000

    SG-391 Add targets to outposts

---
+++ main_game/Assets/Scripts/Outposts/OutpostTarget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutpostTarget : MonoBehaviour 
+{
+    private GameObject player;
+    private MeshRenderer renderer;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    player = GameObject.Find("CameraManager(Clone)");
+        renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if(distance < 600)
+            renderer.enabled = false;
+        else
+            renderer.enabled = true;
+
+		Vector3 v3 = player.transform.position - transform.position;
+		transform.rotation = Quaternion.LookRotation(-v3);
+	}
+}
