ebd0a74c275540f1c68a4f165a273396a2570b1e
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-06 13:08:26 +0000

    SG-343 Add debug info to FollowReticule

--- main_game/Assets/Scripts/Player/FollowReticule.cs
+++ main_game/Assets/Scripts/Player/FollowReticule.cs
@@ -12,12 +12,15 @@ public class FollowReticule : MonoBehaviour
 {
 	#pragma warning disable 0649 // Disable warnings about unset private SerializeFields
 	[SerializeField] private int controlledByPlayerId;
-	#pragma warning restore 0649 
+	#pragma warning restore 0649
 
 	// Empty game object to use as the target position
     private GameObject targetPoint;
     private GameObject crosshairContainer;
     private GameObject[] crosshairs;
+
+    // Draws line between turret position and aim position if true
+    private bool debug = false;
 
     // Initialise temporary object
     void Start()
@@ -44,21 +47,24 @@ public class FollowReticule : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(crosshairs[controlledByPlayerId].transform.position);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit) && !hit.transform.gameObject.tag.Equals("Player"))
+            if (Physics.Raycast(ray, out hit) && (hit.transform.gameObject.tag.Equals("Debris") || hit.transform.gameObject.tag.Equals("EnemyShip")))
                 targetPoint.transform.position = hit.transform.position;
             else
             {
-                targetPoint.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(crosshairs[controlledByPlayerId].transform.position.x, crosshairs[controlledByPlayerId].transform.position.y, 0));
-                targetPoint.transform.Translate(transform.parent.transform.forward * (-1000f));
+                targetPoint.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(crosshairs[controlledByPlayerId].transform.position.x, crosshairs[controlledByPlayerId].transform.position.y, 1000));
+                targetPoint.transform.Translate(transform.parent.transform.forward * (-10f));
             }
 
-            transform.LookAt(targetPoint.transform.position);
+            //transform.LookAt(targetPoint.transform.position);
             Quaternion targetRotation = Quaternion.LookRotation(targetPoint.transform.position - transform.position);
-
             transform.rotation = targetRotation;
 
             // Angle correction
             transform.localEulerAngles = new Vector3(270f, transform.localEulerAngles.y - 90f, transform.localEulerAngles.z);
+
+            // Draw line between turret position and aim position. Gizmos needs to be enabled in Game View to see the line (click Gizmos in top right)
+            if(debug)
+                Debug.DrawRay(transform.position, targetPoint.transform.position - transform.position, Color.green);
         }
     }
 }
