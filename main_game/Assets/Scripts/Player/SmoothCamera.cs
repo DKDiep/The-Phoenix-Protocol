6252074ffd4c35017aa3b0a1ebd4a098fe3ec842
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-09 19:25:20 +0000

    SG-419 Re-add smooth camera script

---
+++ main_game/Assets/Scripts/Player/SmoothCamera.cs
@@ -0,0 +1,32 @@
+﻿/*
+    2015-2016 Team Pyrolite
+    Project "Sky Base"
+    Authors: Marc Steene
+    Description: Smooth camera movement
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour 
+{
+
+GameObject parent;
+[SerializeField] float damping;
+    
+    // Update is called once per frame
+    void LateUpdate () 
+    {
+        if (parent != null)
+        {
+            transform.position = parent.transform.position;
+            Quaternion rotation = parent.transform.rotation;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+        }
+        else
+        {
+            transform.parent = null; // unlink from parent
+            parent = GameObject.Find("PlayerShip(Clone)");
+        }
+    }
+}
