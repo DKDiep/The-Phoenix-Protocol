1cb7f39cecff6f4c9a645f020488d3751b29d1c4
lukebryant <mb13273@my.bristol.ac.uk>
2016-03-17 14:42:11 +0000

    Add map panel to commandermanager prefab

---
+++ main_game/Assets/StratMap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StratMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var panel = this;
+        if (panel != null)  // make sure you actually found it!
+        {
+            GameObject outPostSymbol = Instantiate(Resources.Load("Prefabs/IndicatorArrow", typeof(GameObject))) as GameObject;
+            outPostSymbol.transform.SetParent(panel.transform, false);
+            RectTransform arrowRectTransform = (RectTransform)outPostSymbol.transform;
+            Vector3 screenPos = new Vector3(0, 0,0);
+            arrowRectTransform.anchoredPosition = screenPos;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
