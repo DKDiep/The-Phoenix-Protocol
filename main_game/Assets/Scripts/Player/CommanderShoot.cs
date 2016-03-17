52faee122b5d6eb3d985be42ed33ec3b37271d1e
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-17 20:19:27 +0300

    SG-248 Remove unnecessary assets from the project

---
+++ main_game/Assets/Scripts/Player/CommanderShoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommanderShoot : MonoBehaviour {
+
+    [SerializeField] float cooldown;
+    private bool canShoot = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetMouseButtonDown(0) && canShoot)
+        {
+            canShoot = false;
+            StartCoroutine("Cooldown");
+        }
+            
+	}
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+    }
+
+}
