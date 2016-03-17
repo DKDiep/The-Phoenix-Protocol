32fa296e09bdcc4c01d65d172cfc7f374d812ab3
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-17 13:38:39 +0300

    SG-472 Remove enemy targets when too close

--- main_game/Assets/Scripts/Enemies/TargetScript.cs
+++ main_game/Assets/Scripts/Enemies/TargetScript.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class TargetScript : MonoBehaviour 
 {
     private GameObject player;
+    private float distance; // Distance to the player
+    private Renderer renderer;
 
 	// Use this for initialization
 	void Start () 
     {
 	    player = GameObject.Find("CameraManager(Clone)");
+        renderer = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -16,5 +19,12 @@ public class TargetScript : MonoBehaviour
     {
 		Vector3 v3 = player.transform.position - transform.position;
 		transform.rotation = Quaternion.LookRotation(-v3);
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if(distance > 800f || distance < 25f)
+            renderer.enabled = false;
+        else
+            renderer.enabled = true;
+            
 	}
 }
