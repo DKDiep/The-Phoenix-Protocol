9f1719d182a491b62d3e80ba421b1a9457a911a6
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-20 17:24:52 +0300

    SG-511 Add multiple rocket spawning

--- main_game/Assets/Scripts/Player/CommanderAbilities/ShootingAbility.cs
+++ main_game/Assets/Scripts/Player/CommanderAbilities/ShootingAbility.cs
@@ -40,21 +40,25 @@ public class ShootingAbility : CommanderAbility {
         if(impactManager == null)
             impactManager      = GameObject.Find("AsteroidExplosionManager").GetComponent<ObjectPoolManager>();
 
-        GameObject obj = bulletManager.RequestObject();
-        obj.transform.position = shootAnchor.transform.position;
-        obj.transform.localScale = new Vector3(5f,5f,5f);
+        for(int i = 0; i < settings.projectileCount; i++)
+        {
+            GameObject obj = bulletManager.RequestObject();
+            obj.transform.position = shootAnchor.transform.position;
+            obj.transform.localScale = new Vector3(5f,5f,5f);
 
-        GameObject logic = logicManager.RequestObject();
-        BulletLogic logicComponent = logic.GetComponent<BulletLogic>();
-        logicComponent.SetParameters(0.1f, 250f, 5f);
+            GameObject logic = logicManager.RequestObject();
+            BulletLogic logicComponent = logic.GetComponent<BulletLogic>();
+            logicComponent.SetParameters(0.1f, 250f, 5f);
 
-        logic.transform.parent = obj.transform;
-        target.transform.position = shootAnchor.transform.position;
-        target.transform.Translate(transform.forward * 1000f);
+            logic.transform.parent = obj.transform;
+            target.transform.position = shootAnchor.transform.position;
+            target.transform.Translate(transform.forward * 1000f);
 
-        logicComponent.SetDestination(target.transform.position, true, this.gameObject, bulletManager, logicManager, impactManager);
+            logicComponent.SetDestination(target.transform.position, true, this.gameObject, bulletManager, logicManager, impactManager);
 
-        bulletManager.EnableClientObject(obj.name, obj.transform.position, obj.transform.rotation, obj.transform.localScale);
+            bulletManager.EnableClientObject(obj.name, obj.transform.position, obj.transform.rotation, obj.transform.localScale);
+        }
+
 
     }
 
