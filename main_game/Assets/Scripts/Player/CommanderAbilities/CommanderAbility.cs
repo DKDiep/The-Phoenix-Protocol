0b6609f1cefe500dd6086eec5a4f1bd9268ee673
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-20 14:02:25 +0300

    SG-473 Add base classes

---
+++ main_game/Assets/Scripts/Player/CommanderAbilities/CommanderAbility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CommanderAbility : MonoBehaviour {
+
+    internal float cooldown;
+    internal bool ready = true;
+
+    internal abstract void AbilityEffect();
+
+    internal IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        ready = true;
+    }
+
+    internal void UseAbility()
+    {
+        if(ready)
+        {
+            ready = false;
+            CoolDown();
+            AbilityEffect();
+        }
+     }
+	
+}
---
+++ main_game/Assets/Scripts/Player/CommanderAbilities/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : CommanderAbility {
+
+    [SerializeField] float readyDelay;
+
+	// Use this for initialization
+	private void Awake () 
+    {
+        cooldown = readyDelay;
+	}
+
+	// Update is called once per frame
+	private void Update () 
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            UseAbility();
+        }
+	}
+
+    internal override void AbilityEffect()
+    {
+        Debug.Log("Shoot");
+    }
+
+
+}
