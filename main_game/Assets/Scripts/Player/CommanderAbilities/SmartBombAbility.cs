f1b7a303223ffeab9ed14a974fff70ffc9989eb8
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-20 16:20:17 +0300

    SG-503 Add smart bomb script

---
+++ main_game/Assets/Scripts/Player/CommanderAbilities/SmartBombAbility.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmartBombAbility : CommanderAbility {
+
+    private float originalSpeed;
+
+	// Use this for initialization
+	private void Awake () 
+    {
+        settings = GameObject.Find("GameSettings").GetComponent<GameSettings>();
+        cooldown = settings.boostCooldown;
+        duration = settings.boostDuration;
+        state = GameObject.Find("GameManager").GetComponent<GameState>();
+	}
+
+	// Update is called once per frame
+	private void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            UseAbility();
+        }
+	}
+
+    internal override void ActivateAbility()
+    {
+        Debug.Log("Boost ability enabled");
+        originalSpeed = state.GetShipSpeed();
+        state.SetShipSpeed (originalSpeed * settings.boostSpeedMultiplier);
+    }
+
+    internal override void DeactivateAbility()
+    {
+        Debug.Log("Boost ability disabled");
+        state.SetShipSpeed(originalSpeed);
+    }
+
+
+}
