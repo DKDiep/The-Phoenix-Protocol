ec6ca6fd1a88a7991d473f3ca18d0dab104344bb
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-03-20 17:08:11 +0000

    SG-467 Fix stats not showing at the end of the game.

--- main_game/Assets/Scripts/Network/GameStatusManager.cs
+++ main_game/Assets/Scripts/Network/GameStatusManager.cs
@@ -27,39 +27,47 @@ public class GameStatusManager : NetworkBehaviour
 		if((gameState.Status == GameState.GameStatus.Died ||
 			gameState.Status == GameState.GameStatus.Won) && !gameOverScreen)
 		{
-            if(musicManager == null)
-                musicManager = GameObject.Find("MusicManager(Clone)").GetComponent<MusicManager>();
-
-            GameObject gameTimer = GameObject.Find("GameTimerText");
-            gameTimer.SetActive(false);
-
-            GameObject.Find("Portal(Clone)").SetActive(false);
 
             // Set an overlay on the screen
 			gameOverCanvas = Instantiate(Resources.Load("Prefabs/GameOverCanvas", typeof(GameObject))) as GameObject;
 			if(gameState.Status == GameState.GameStatus.Died) 
             {
                 gameOverCanvas.transform.Find("StatusText").gameObject.GetComponent<Text>().text = "Your ship and the crew were killed.";
-                musicManager.PlayMusic(2);
             }
-
 			else
             {
                 gameOverCanvas.transform.Find("StatusText").gameObject.GetComponent<Text>().text = "You reached the portal!";
-                musicManager.PlayMusic(2);
             }
-
 
             if(playerController.GetRole() == RoleEnum.Camera)
             {
-                // If it is the server, disable stats
+                // Disable the portal on all screens
+                GameObject.Find("Portal(Clone)").SetActive(false);
+
+                // If it is the server
                 if(playerController.netId.Value == server.GetComponent<ServerManager>().GetServerId())
                 {
+                    // Do not display stats on server.
                     gameOverCanvas.transform.Find("GameOverStats").gameObject.SetActive(false);
                     Debug.Log("Final Game Score: " + this.gameObject.GetComponent<GameStatsManager>().CalculateAndSendGameScore());
+
+                  
+                    // Manage end game music
+                    if(musicManager == null)
+                        musicManager = GameObject.Find("MusicManager(Clone)").GetComponent<MusicManager>();
+
+                    if(gameState.Status == GameState.GameStatus.Died) 
+                        musicManager.PlayMusic(2);
+                    else
+                        musicManager.PlayMusic(2);
+
+                    // Disable game timer. 
+                    GameObject gameTimer = GameObject.Find("GameTimerText");
+                    gameTimer.SetActive(false);
                 }
                 else 
                 {
+                    // Show stats on side screens.
                     gameOverCanvas.transform.Find("StatusText").gameObject.SetActive(false);
                     gameOverCanvas.transform.Find("GameOverText").gameObject.SetActive(false);
                 }
