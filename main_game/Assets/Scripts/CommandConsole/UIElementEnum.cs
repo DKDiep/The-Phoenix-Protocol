e41c1d7b75dd2864ebce44835eb2f20a44fb6574
lukebryant <mb13273@my.bristol.ac.uk>
2016-04-05 18:17:20 +0100

    SG-722 Add functionality for UI elements to pulsate

---
+++ main_game/Assets/Scripts/CommandConsole/UIElementEnum.cs
@@ -0,0 +1,12 @@
+﻿public enum UIElementEnum { //note the values of the different upgrade types correspond to the values in ComponentType.cs. This fact is used in CommandConsoleState.addUpgradeBoxes
+    ShieldsUpgrade = 0,
+    TurretsUpgrade = 1,
+    EngineUpgrade = 2,
+    BridgeUpgrade = 3,
+    DroneUpgrade = 4,
+    ResourceStorageUpgrade = 5,
+    UpgradeInfo = 6,
+    NewsFeed = 7,
+    MissionWindow = 8,
+    MapBG = 9
+}
