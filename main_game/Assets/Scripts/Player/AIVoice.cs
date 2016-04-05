ed473df04eb6ad68e3649e922e1136ee48475150
marc_steene <ms13829@my.bristol.ac.uk>
2016-04-05 15:26:49 +0300

    SG-727 Add sound files

---
+++ main_game/Assets/Scripts/Player/AIVoice.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIVoice : MonoBehaviour {
+
+    [SerializeField] AudioClip[] aiClips;
+    private AudioSource mySource;
+
+    private void Start()
+    {
+        mySource = GetComponent<AudioSource>();
+    }
+
+	public void PlaySound(int id)
+    {
+        mySource.PlayOneShot(aiClips[id]);
+    }
+}
