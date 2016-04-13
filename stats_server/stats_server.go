01b6c73ad17941aadd9a266151f899f32547000f
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-04-13 09:17:06 +0100

    SG-776 Pull spectator data from database and serve via /get_spectator_data

--- stats_server/stats_server.go
+++ stats_server/stats_server.go
@@ -24,28 +24,46 @@ type Scores struct {
 	Score    int    `json:"score"`
 }
 
+type Player struct {
+    Name   string    `json:"name"`
+    Score  int       `json:"score"`
+}
+
 func main() {
 	fmt.Println("Starting Stats Server..")
 	dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
+
 	db, err := sql.Open("mysql", dsn)
+    defer db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	stmt, err := db.Prepare("INSERT game SET team_name=?, scores=?")
+
+
+    // Prepare query for inserting into game
+	saveGameStmt, err := db.Prepare("INSERT game SET team_name=?, scores=?")
+    defer saveGameStmt.Close()
 	if err != nil {
 		panic(err.Error())
 	}
-	defer stmt.Close()
-	JSONString := ""
+
+    // Prepare query for getting spectator
+    spectatorStmt, err := db.Prepare("SELECT username, score FROM players")
+    defer spectatorStmt.Close()
+    if err != nil {
+        panic(err.Error())
+    }
+
+	mainGameJson := ""
+
 	//Handles game data which is sent periodiocally by the main game
 	http.HandleFunc("/game_data", func(w http.ResponseWriter, r *http.Request) {
 		byteArray, err1 := ioutil.ReadAll(r.Body)
 		decodedString, err2 := url.QueryUnescape(string(byteArray))
 		if err1 == nil {
 			if err2 == nil {
-				JSONString = strings.TrimPrefix(decodedString, "JSON:=")
-				fmt.Println(JSONString)
+				mainGameJson = strings.TrimPrefix(decodedString, "JSON:=")
+				fmt.Println(mainGameJson)
 			} else {
 				log.Fatal(err2)
 			}
@@ -53,10 +71,44 @@ func main() {
 			log.Fatal(err1)
 		}
 	})
+
 	http.Handle("/", http.FileServer(http.Dir("../web/stats_web")))
+
+
 	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "%q", JSONString)
+		fmt.Fprintf(w, "%q", mainGameJson)
 	})
+
+
+    http.HandleFunc("/get_spectator_data", func(w http.ResponseWriter, r *http.Request) {
+        var (
+        	name string
+        	score int
+        )
+        var players []Player;
+        rows, err := spectatorStmt.Query()
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+        for rows.Next() {
+            err := rows.Scan(&name, &score)
+        	if err != nil {
+        		log.Fatal(err)
+        	}
+            players = append(players, Player{
+                Name:   name,
+                Score:  score,
+            })
+        }
+        if err = rows.Err(); err != nil {
+            log.Fatal(err)
+        }
+        spectatorGameJson, _ := json.Marshal(players)
+        fmt.Fprintf(w, "%s", string(spectatorGameJson))
+    })
+
+
 	//Handles data which is to be saved between games
 	http.HandleFunc("/save_game_data", func(w http.ResponseWriter, r *http.Request) {
 		var trimmedByteArray []byte
@@ -64,9 +116,9 @@ func main() {
 		decodedString, err2 := url.QueryUnescape(string(byteArray))
 		if err1 == nil {
 			if err2 == nil {
-				JSONString = strings.TrimPrefix(decodedString, "JSON:=")
-				fmt.Println(JSONString)
-				trimmedByteArray = []byte(JSONString)
+				mainGameJson = strings.TrimPrefix(decodedString, "JSON:=")
+				fmt.Println(mainGameJson)
+				trimmedByteArray = []byte(mainGameJson)
 			} else {
 				log.Fatal(err2)
 			}
@@ -75,12 +127,11 @@ func main() {
 		}
 		var scores Scores
 		err := json.Unmarshal(trimmedByteArray, &scores)
-		res, err := stmt.Exec(scores.TeamName, scores.Score)
+		_, err = saveGameStmt.Exec(scores.TeamName, scores.Score)
 		if err != nil {
 			panic(err.Error())
-			fmt.Println(res)
 		}
 	})
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
-	// Prepare statement for reading data
 }
