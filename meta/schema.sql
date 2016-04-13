2bd725f35a3eb5812d6a2a01214d59e25c4faf5d
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-04-13 09:35:36 +0100

    SG-777 Add last updated time and field to detect if in main game.

--- meta/schema.sql
+++ meta/schema.sql
@@ -31,15 +31,6 @@ CREATE TABLE `game` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `game`
---
-
-LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `players`
 --
 
@@ -51,18 +42,11 @@ CREATE TABLE `players` (
   `username` varchar(30) NOT NULL,
   `score` int(11) unsigned DEFAULT NULL,
   `registered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `in_main_game` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`player_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `players`
---
-
-LOCK TABLES `players` WRITE;
-/*!40000 ALTER TABLE `players` DISABLE KEYS */;
-/*!40000 ALTER TABLE `players` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -73,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-11 17:04:10
+-- Dump completed on 2016-04-13  9:32:45
