81ab0389a99ff43e4c23879a27e875caea6e82c0
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-04-14 00:32:20 +0100

    SG-798 Significantly reduce delay on outpost arrows
    This stops the lagging of the arrows, looks like it doesn't take any performance hit, ran with the profilers and everything looks fine.

--- main_game/Assets/Scripts/Outposts/OutpostManager.cs
+++ main_game/Assets/Scripts/Outposts/OutpostManager.cs
@@ -65,7 +65,7 @@ public class OutpostManager : MonoBehaviour {
             DiscoverOutposts(); 
             UpdateOutpostArrows(); 
         }
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.01f);
         StartCoroutine(UpdateOutposts());
     }
 
