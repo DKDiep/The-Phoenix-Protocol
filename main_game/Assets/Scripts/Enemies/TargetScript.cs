a9c3baf13bb76fbae18a1e9c4b4ae866a6bd78e3
marc_steene <ms13829@my.bristol.ac.uk>
2016-04-19 19:47:35 +0100

    SG-893 Remove targets from enemies and outposts on engineer

--- main_game/Assets/Scripts/Enemies/TargetScript.cs
+++ main_game/Assets/Scripts/Enemies/TargetScript.cs
@@ -6,11 +6,18 @@ public class TargetScript : MonoBehaviour
     private GameObject player;
     private float distance; // Distance to the player
     private new Renderer renderer;
+    private bool isEngineer;
 
     void OnEnable()
     {
         if(player == null)
-            player = GameObject.Find("CameraManager(Clone)");
+        {
+            if(GameObject.Find("CameraManager(Clone)") != null)
+                player = GameObject.Find("CameraManager(Clone)");
+            else
+                Destroy(this);
+        }
+
         if(renderer == null)
             renderer = GetComponent<Renderer>();
         StartCoroutine(UpdateDistance()); 
@@ -19,18 +26,21 @@ public class TargetScript : MonoBehaviour
 
     IEnumerator UpdateDistance()
     {
-        distance = Vector3.Distance(transform.position, player.transform.position);
-        if(distance > 650f || distance < 125f)
-            renderer.enabled = false;
-        else
+        if(player != null)
         {
-            Vector3 v3 = player.transform.position - transform.position;
-            transform.rotation = Quaternion.LookRotation(-v3);
-            renderer.enabled = true;
-            if(Random.Range(0,1000) == 0)
-                AIVoice.SendCommand(Random.Range(19,22));
+            distance = Vector3.Distance(transform.position, player.transform.position);
+            if(distance > 650f || distance < 125f)
+                renderer.enabled = false;
+            else
+            {
+                Vector3 v3 = player.transform.position - transform.position;
+                transform.rotation = Quaternion.LookRotation(-v3);
+                renderer.enabled = true;
+                if(Random.Range(0,1000) == 0)
+                    AIVoice.SendCommand(Random.Range(19,22));
+            }
+            yield return new WaitForSeconds(Mathf.Clamp(distance / 750f, 0.1f, 1f));
+            StartCoroutine(UpdateDistance());
         }
-        yield return new WaitForSeconds(Mathf.Clamp(distance / 750f, 0.1f, 1f));
-        StartCoroutine(UpdateDistance());
     }
 }
---
+++ main_game/Assets/Scripts/Engineer/EngineerRendering.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineerRendering : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+    {
+        GameObject.Find("TargetCamera").SetActive(false);
+        Camera.main.gameObject.name = "CommanderCamera";
+	}
+}
