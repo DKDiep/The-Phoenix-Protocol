82d20073fe8e20b0e7c5c8573fcd056249edf30b
lukebryant <mb13273@my.bristol.ac.uk>
2016-04-19 12:31:01 +0100

    SG-888 hard-coded new starting position on reset

--- main_game/Assets/Scripts/Network/ServerManager.cs
+++ main_game/Assets/Scripts/Network/ServerManager.cs
@@ -436,7 +436,7 @@ public class ServerManager : NetworkBehaviour
         GameObject.Find("OutpostManager(Clone)").GetComponent<OutpostManager>().Reset();
 
         // Reset player ship
-        gameState.PlayerShip.transform.position = new Vector3(0.0f,0.0f,-2000.0f);
+        gameState.PlayerShip.transform.position = new Vector3(-2000.0f,0.0f,-2000.0f);
         gameState.PlayerShip.transform.rotation = Quaternion.identity;
         gameState.PlayerShip.GetComponentInChildren<PlayerShooting>().Reset();
         gameState.PlayerShip.GetComponentInChildren<ShipMovement>().Reset();
