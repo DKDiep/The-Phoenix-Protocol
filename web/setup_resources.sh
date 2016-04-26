febc6cbc33ec98688c3979a7fb058aaf8ef34346
frank_hemsworth <fh13286@my.bristol.ac.uk>
2016-04-26 12:09:13 +0100

    SG-992 Created QR code and wifi connection details

--- web/setup_resources.sh
+++ web/setup_resources.sh
@@ -1,7 +1,9 @@
 ln -s ../resources/include/ ./phone_web/include
 ln -s ../resources/include/ ./stats_web/include
 ln -s ../resources/include/ ./admin_web/include
+ln -s ../resources/include/ ./instructions_web/include
 
 ln -s ../resources/fonts/ ./phone_web/fonts
 ln -s ../resources/fonts/ ./stats_web/fonts
 ln -s ../resources/fonts/ ./admin_web/fonts
+ln -s ../resources/fonts/ ./instructions_web/fonts
