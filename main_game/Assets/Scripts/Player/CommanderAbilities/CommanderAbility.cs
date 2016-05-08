81e02882957e60659ef05f4ed53be86aa6d4932f
lukebryant <mb13273@my.bristol.ac.uk>
2016-05-08 15:12:33 +0100

    SG-1124 fix bug

--- main_game/Assets/Scripts/Player/CommanderAbilities/CommanderAbility.cs
+++ main_game/Assets/Scripts/Player/CommanderAbilities/CommanderAbility.cs
@@ -40,7 +40,6 @@ public abstract class CommanderAbility : MonoBehaviour {
 
         if(ready)
         {
-            state.shootingUsed = true;
             if(audioSource == null)
                 audioSource = GetComponent<AudioSource>();
             if(soundEffect != null)
